57ff39a398a69ecb5ce3a352bddd7c8c959c0fb1
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2024-12-05 10:51:52 +0100

    modernizing

--- src/GenerateTagger.cxx
+++ src/GenerateTagger.cxx
@@ -80,8 +80,8 @@ namespace Tagger {
 
   template <typename T1, typename T2>
   struct more_second {
-    typedef pair<T1, T2> type;
-    bool operator ()(type const& a, type const& b) const {
+    using c_type = pair<T1, T2>;
+    bool operator ()(c_type const& a, c_type const& b) const {
       return a.second > b.second;
     }
   };
